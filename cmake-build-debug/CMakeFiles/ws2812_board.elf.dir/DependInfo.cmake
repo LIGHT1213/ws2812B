a4b42f1b487e1c3a9ee95987ae7ea70d4d6ecdb8
light <pch19980807@gmail.com>
2021-07-05 21:24:43 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/DependInfo.cmake
@@ -0,0 +1,86 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/startup/startup_stm32f070x6.s" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/startup/startup_stm32f070x6.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F070x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../UserFile/OLED"
+  "../UserFile"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/dma.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/dma.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/gpio.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/gpio.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/i2c.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/i2c.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/main.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/main.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/spi.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/spi.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/stm32f0xx_hal_msp.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/stm32f0xx_hal_msp.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/stm32f0xx_it.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/stm32f0xx_it.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/syscalls.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/syscalls.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/system_stm32f0xx.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/system_stm32f0xx.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/tim.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/tim.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Core/Src/usart.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Core/Src/usart.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi_ex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi_ex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/UserFile/OLED/oled.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/UserFile/OLED/oled.c.obj"
+  "C:/Users/pan39/Desktop/singalchip/ws2812_board/UserFile/ws2812b.c" "C:/Users/pan39/Desktop/singalchip/ws2812_board/cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/UserFile/ws2812b.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F070x6"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Core/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc"
+  "../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy"
+  "../Drivers/CMSIS/Device/ST/STM32F0xx/Include"
+  "../Drivers/CMSIS/Include"
+  "../UserFile/OLED"
+  "../UserFile"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ws2812_board.elf.dir/cmake_clean.cmake
@@ -0,0 +1,43 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/dma.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/gpio.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/i2c.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/main.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/spi.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/stm32f0xx_hal_msp.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/stm32f0xx_it.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/syscalls.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/system_stm32f0xx.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/tim.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Core/Src/usart.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_cortex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_dma.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_exti.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_flash_ex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_gpio.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_i2c_ex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr_ex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_rcc_ex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_spi_ex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim_ex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_uart_ex.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/UserFile/OLED/oled.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/UserFile/ws2812b.c.obj"
+  "CMakeFiles/ws2812_board.elf.dir/startup/startup_stm32f070x6.s.obj"
+  "ws2812_board.elf"
+  "ws2812_board.elf.manifest"
+  "ws2812_board.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/ws2812_board.elf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
